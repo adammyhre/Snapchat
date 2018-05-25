ae9ee630619a38e1c816f24e6e6af4606e519dcd
Adam Myhre <adam@undergalaxie.com>
2018-05-25 10:21:20 -0700

    Cleaned up git

--- Snapchat/SignInViewController.swift
+++ Snapchat/SignInViewController.swift
@@ -1,12 +1,13 @@
 //
-//  ViewController.swift
+//  SignUpViewController.swift
 //  Snapchat
 //
-//  Created by Bradley Myhre on 2018-05-24.
+//  Created by Adam Myhre on 2018-05-24.
 //  Copyright © 2018 undergalaxie.com. All rights reserved.
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
 
@@ -18,7 +19,28 @@ class SignInViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func turnUpTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!, completion: {(user, error) in
+        print ("We tried to sign in")
+            if error != nil {
+                print ("Hey, we have an error!")
+                Auth.auth().createUser(withEmail: self.emailTextfield.text!, password: self.passwordTextfield.text!, completion: {(user, error) in
+                    print ("We tried to create a user")
+                    
+                    if error != nil {
+                        print ("Hey, we have an error!")
+                   
+                    } else {
+                        print ("Created user successfully")
+                        self.performSegue(withIdentifier: "signinsegue", sender: nil)
+                  }
+                })
+            } else {
+                print ("Signed in successfully")
+                self.performSegue(withIdentifier: "signinsegue", sender: nil)
+            }})
+        
+    }
 
-    @IBOutlet weak var turnUpTapped: UIButton!
 }
 
